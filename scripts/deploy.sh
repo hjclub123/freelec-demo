72fb74606983c2fec97f7095d2ecc7da7495d72c
hyun jin kim <khj@hubilon.com>
2023-01-08 22:47:38 +0900

    업데이트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,16 +8,20 @@ function pause() {
 
 PROJECT_NAME=freelec-demo
 
+set +x
 echo "---------------------------------------------------------"
 pause "CI 1. Project Test, Build - Press any key to continue..."
 echo "---------------------------------------------------------"
+set -x
 
 git pull
 ./gradlew clean build
 
+set +x
 echo "---------------------------------------------------------"
 pause "CI 2. Before Deploy - Press any key to continue..."
 echo "---------------------------------------------------------"
+set -x
 
 mkdir -p before-deploy
 cp scripts/*.sh before-deploy/
@@ -29,27 +33,36 @@ mkdir  ../deploy
 mv before-deploy/before-deploy.tar ../deploy/${PROJECT_NAME}.tar
 rm -rf before-deploy
 
+set +x
 echo "---------------------------------------------------------"
 pause 'CD 3. Deploy - Press any key to continue...'
 echo "---------------------------------------------------------"
+set -x
 
 cd ../deploy
 tar xvf ${PROJECT_NAME}.tar
 
+set +x
 echo "---------------------------------------------------------"
 pause 'CD 4. Stop - Press any key to continue...'
 echo "---------------------------------------------------------"
+set -x
 
 ./stop.sh
 
+set +x
 echo "---------------------------------------------------------"
 pause 'CD 4. Start - Press any key to continue...'
 echo "---------------------------------------------------------"
+set -x
 
 ./start.sh
 
+set +x
 echo "---------------------------------------------------------"
 pause 'CD 4. Health Check - Press any key to continue...'
 echo "---------------------------------------------------------"
+set -x
 
 ./health.sh
+
